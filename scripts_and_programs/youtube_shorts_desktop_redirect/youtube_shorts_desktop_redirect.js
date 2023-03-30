8c46f92eba30db3c19947e6a19ac699b989199b7
kuuube <hexagonisalie@gmail.com>
2023-03-30 15:29:39 -0400

    Youtube Shorts Desktop Redirect add shorts link replacement

--- scripts_and_programs/youtube_shorts_desktop_redirect/youtube_shorts_desktop_redirect.js
+++ scripts_and_programs/youtube_shorts_desktop_redirect/youtube_shorts_desktop_redirect.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Youtube Shorts Desktop Redirect
 // @namespace    https://github.com/Kuuuube/Misc_Scripts/tree/main/scripts_and_programs/youtube_shorts_desktop_redirect
-// @version      0.2
+// @version      0.3
 // @description  Redirects youtube shorts to the desktop player.
 // @author       Kuuube
 // @match        *://www.youtube.com/*
@@ -11,12 +11,35 @@
 function redirect() {
     var url = window.location.pathname;
     if (url.includes("shorts/")) {
-        window.stop()
+        window.stop();
         window.location.replace(url.replace("shorts/", "watch?v="));
+    }
+}
+
+var replace_check = true;
+
+function replace() {
+    var links = document.getElementsByTagName("a");
+
+    for (var i = 0; i < links.length; i++) {
+        links[i].href = links[i].href.replace("shorts/", "watch?v=");
+    }
+
+    replace_check = true;
+}
+
+function debounce(method, delay) {
+    if (replace_check) {
+        method.debounce_timer = setTimeout(function() {method()}, delay);
+        replace_check = false;
     }
 }
 
 (function() {
     redirect();
-    window.addEventListener("yt-navigate-start", function() {redirect()})
+    window.addEventListener("yt-navigate-start", function() {redirect()});
+
+    replace();
+    window.addEventListener("DOMSubtreeModified", function() {debounce(replace, 100)});
+    window.addEventListener("yt-navigate-finish", function() {replace()});
 })();
