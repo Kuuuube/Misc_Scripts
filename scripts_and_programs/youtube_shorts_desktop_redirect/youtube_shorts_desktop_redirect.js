c6f2c2224bbd1bcb4d0e13dcc21db55831e2070e
kuuuube <hexagonisalie@gmail.com>
2023-04-11 14:50:22 -0400

    Youtube Shorts Desktop Redirect: Use full url for replace checking

--- scripts_and_programs/youtube_shorts_desktop_redirect/youtube_shorts_desktop_redirect.js
+++ scripts_and_programs/youtube_shorts_desktop_redirect/youtube_shorts_desktop_redirect.js
@@ -1,16 +1,16 @@
 // ==UserScript==
 // @name         Youtube Shorts Desktop Redirect
 // @namespace    https://github.com/Kuuuube/Misc_Scripts/tree/main/scripts_and_programs/youtube_shorts_desktop_redirect
-// @version      0.5
+// @version      0.6
 // @description  Redirects youtube shorts to the desktop player.
 // @author       Kuuube
-// @run-at document-start
+// @run-at       document-start
 // @match        *://www.youtube.com/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=youtube.com
 // ==/UserScript==
 
 function redirect() {
-    var url = window.location.pathname;
+    var url = window.location.origin + window.location.pathname;
     if (url.includes("youtube.com/shorts/")) {
         window.stop();
         window.location.replace(url.replace("youtube.com/shorts/", "youtube.com/watch?v="));
