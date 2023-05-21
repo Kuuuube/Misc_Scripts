6e68e1131c9b46f60f0eb7300274b66b024c9a38
kuuuube <hexagonisalie@gmail.com>
2023-05-21 01:44:11 -0400

    Word compressor remove sorting

--- scripts_and_programs/clemenpine_word_list_compressor/haskell/app/Main.hs
+++ scripts_and_programs/clemenpine_word_list_compressor/haskell/app/Main.hs
@@ -6,7 +6,7 @@ import Data.List.Split (splitOn)
 import Debug.Trace (traceShow)
 import Data.Time (getCurrentTime, diffUTCTime)
 import Data.Text (Text, drop, take, length, pack, unpack)
-import Data.Sequence (Seq, fromList, sort, deleteAt, (><))
+import Data.Sequence (Seq, fromList, deleteAt, length, (><))
 import Data.Foldable (toList)
 
 trigrams :: Seq Text -> Seq Text
@@ -16,16 +16,13 @@ trigrams_map :: Text -> [Text]
 trigrams_map input_string = [Data.Text.take 3 (Data.Text.drop offset input_string) | offset <- [0..(Data.Text.length input_string - 3)]]
 
 between_word_trigrams :: Seq Text -> Seq Text
-between_word_trigrams input_list = do
-    let starts_and_ends_list = concatMap between_word_trigrams_map input_list
-    let starts_and_ends = fromList (nub starts_and_ends_list)
-    starts_and_ends
+between_word_trigrams input_list = fromList (nub (concatMap between_word_trigrams_map input_list))
 
 between_word_trigrams_map :: Text -> [Text]
 between_word_trigrams_map input_text = [Data.Text.take 1 (Data.Text.drop 1 input_text), Data.Text.drop (Data.Text.length input_text - 2) input_text]
 
 get_trigrams :: Seq Text -> Seq Text
-get_trigrams words_seq = sort (trigrams words_seq >< between_word_trigrams words_seq)
+get_trigrams words_seq = trigrams words_seq >< between_word_trigrams words_seq
 
 try_remove :: Int -> Int -> Seq Text -> Seq Text -> Seq Text
 try_remove word_index end_index all_words trigrams_seq = do
@@ -33,7 +30,7 @@ try_remove word_index end_index all_words trigrams_seq = do
         all_words
     else do
         let new_seq = deleteAt word_index all_words
-        if trigrams_seq == get_trigrams new_seq then do
+        if Data.Sequence.length trigrams_seq == Data.Sequence.length (get_trigrams new_seq) then do
             try_remove word_index (end_index - 1) new_seq trigrams_seq
         else do
             try_remove (word_index + 1) end_index all_words trigrams_seq
