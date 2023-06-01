6b3d072f7803601b962a1166809a2e8ab7d25109
kuuuube <hexagonisalie@gmail.com>
2023-06-01 12:47:26 -0400

    Wikipedia Full Width Always remove button, remove popup, and set cookie

--- scripts_and_programs/wikipedia_full_width_always/wikipedia_full_width_always.js
+++ scripts_and_programs/wikipedia_full_width_always/wikipedia_full_width_always.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Wikipedia Full Width Always
 // @namespace    https://github.com/Kuuuube/Misc_Scripts/tree/main/scripts_and_programs/wikipedia_full_width_always
-// @version      0.3
+// @version      0.4
 // @description  Automatically set wikipedia pages to full width.
 // @author       Kuuube
 // @match        *://*.wikipedia.org/*
@@ -22,6 +22,20 @@ function disableLimitedWidth() {
     }
 }
 
+function deletePopupButton() {
+    const popupButton = document.querySelector(".vector-settings");
+    if (popupButton && popupButton.parentElement) {
+        popupButton.parentElement.removeChild(popupButton);
+    }
+}
+
+function setFullWidthCookie() {
+    document.cookie = "enwikimwclientprefs=vector-feature-limited-width"
+    document.cookie = "enwikilimited-width-aware=1";
+}
+
 (function() {
     disableLimitedWidth();
+    deletePopupButton();
+    setFullWidthCookie();
 })();
