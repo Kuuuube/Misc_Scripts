9b1c79187c3dc16008a88b7d92b82d4c9a08f3be
kuuuube <hexagonisalie@gmail.com>
2023-07-24 21:29:07 -0400

    Added kanji conversion

--- scripts_and_programs/jp_number_converter/src/main.rs
+++ scripts_and_programs/jp_number_converter/src/main.rs
@@ -1,11 +1,15 @@
 mod hiragana_tests;
+mod kanji_tests;
+
 mod hiragana;
+mod kanji;
 
 fn main() {
     loop {
         println!("Input: ");
         let input_string = read_line().trim().to_string();
         println!("{}", hiragana::convert_number(&input_string));
+        println!("{}", kanji::convert_number(&input_string));
     }
 }
 
