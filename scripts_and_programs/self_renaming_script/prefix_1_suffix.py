c787fbe3ee9affce8544f4727017ad19d0f6400e
kuuuube <hexagonisalie@gmail.com>
2023-08-21 23:58:05 -0400

    Allow prefix or suffix to be empty

--- scripts_and_programs/self_renaming_script/prefix_1_suffix.py
+++ scripts_and_programs/self_renaming_script/prefix_1_suffix.py
@@ -1,4 +1,4 @@
 import os
 prefix = "prefix_"
 suffix = "_suffix.py"
-os.rename(__file__, __file__.split(prefix)[0] + prefix + str(int(__file__.split(prefix)[1].split(suffix)[0]) + 1) + suffix)
+os.rename(__file__, __file__[::-1].replace(__file__.split("/")[-1].split("\\")[-1][::-1], "", 1)[::-1] + prefix + str(int(__file__.split("/")[-1].split("\\")[-1].replace(prefix, "", 1)[::-1].replace(suffix[::-1], "", 1)[::-1]) + 1) + suffix)
