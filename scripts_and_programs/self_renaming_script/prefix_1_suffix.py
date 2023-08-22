526eb6e2075de325f5f22969e3e484e7fbc09b2d
kuuuube <hexagonisalie@gmail.com>
2023-08-21 23:31:52 -0400

    Move `.py` to suffix

--- scripts_and_programs/self_renaming_script/prefix_1_suffix.py
+++ scripts_and_programs/self_renaming_script/prefix_1_suffix.py
@@ -1,4 +1,4 @@
 import os
 prefix = "prefix_"
-suffix = "_suffix"
-os.rename(__file__, __file__.split(prefix)[0] + prefix + str(int(__file__.split(prefix)[1].split(suffix)[0].split(".py")[0]) + 1) + suffix + ".py")
+suffix = "_suffix.py"
+os.rename(__file__, __file__.split(prefix)[0] + prefix + str(int(__file__.split(prefix)[1].split(suffix)[0]) + 1) + suffix)
