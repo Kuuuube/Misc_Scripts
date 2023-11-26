00d91bddb11cd8596b652e35978c6e9efa6390f9
kuuuube <hexagonisalie@gmail.com>
2023-11-26 17:05:19 -0500

    Replace link end first to avoid potentially risky replace on decoded string

--- scripts_and_programs/duckduckgo_detracker/duckduckgo_detracker.js
+++ scripts_and_programs/duckduckgo_detracker/duckduckgo_detracker.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Duckduckgo Detracker
 // @namespace    https://github.com/Kuuuube/Misc_Scripts/tree/main/scripts_and_programs/duckduckgo_detracker
-// @version      0.1
+// @version      0.2
 // @description  Removes tracker links from direct html.duckduckgo search results.
 // @author       Kuuube
 // @match        https://*.duckduckgo.com/*
@@ -11,6 +11,6 @@
 (function() {
     var links = document.getElementsByTagName("a");
     for (var i = 0; i < links.length; i++) {
-        links[i].href = decodeURIComponent(links[i].href).replaceAll(/duckduckgo.com\/l\/\?uddg\=https:\/\//g, "").replaceAll(/&rut=.*/g, "")
+        links[i].href = decodeURIComponent(links[i].href.replaceAll(/&rut=.*/g, "")).replaceAll(/duckduckgo.com\/l\/\?uddg\=https:\/\//g, "")
     }
 })();
