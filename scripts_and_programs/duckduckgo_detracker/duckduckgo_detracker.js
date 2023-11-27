be5c5804e3d595be137fc256bf5c0b414ee4b055
kuuuube <hexagonisalie@gmail.com>
2023-11-27 16:28:19 -0500

    Fix detrackering on non https links

--- scripts_and_programs/duckduckgo_detracker/duckduckgo_detracker.js
+++ scripts_and_programs/duckduckgo_detracker/duckduckgo_detracker.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Duckduckgo Detracker
 // @namespace    https://github.com/Kuuuube/Misc_Scripts/tree/main/scripts_and_programs/duckduckgo_detracker
-// @version      0.2
+// @version      0.3
 // @description  Removes tracker links from direct html.duckduckgo search results.
 // @author       Kuuube
 // @match        https://*.duckduckgo.com/*
@@ -11,6 +11,6 @@
 (function() {
     var links = document.getElementsByTagName("a");
     for (var i = 0; i < links.length; i++) {
-        links[i].href = decodeURIComponent(links[i].href.replaceAll(/&rut=.*/g, "")).replaceAll(/duckduckgo.com\/l\/\?uddg\=https:\/\//g, "")
+        links[i].href = decodeURIComponent(links[i].href.replaceAll(/&rut=.*/g, "")).replaceAll(/duckduckgo.com\/l\/\?uddg\=.*?:\/\//g, "");
     }
 })();
