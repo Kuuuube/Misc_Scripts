34ed4442bd1b6f547f5e36d405d87f3bc3ab9e1d
kuuuube <hexagonisalie@gmail.com>
2023-12-21 16:00:46 -0500

    Add a few helpful prints

--- scripts_and_programs/tsv_to_freq_dict/tsv_to_freq_dict.py
+++ scripts_and_programs/tsv_to_freq_dict/tsv_to_freq_dict.py
@@ -3,9 +3,9 @@ file_lines = list(map(str.strip, open("list.tsv", "r", encoding="UTF-8").readlin
 freq_rank = 0
 last_freq = int(file_lines[0].split("\t")[1]) + 1 #1 higher than the first row count
 sorted_list = []
+print("Started processing")
 for line in file_lines:
     line_split = line.split("\t")
-    print(int(line_split[1]), last_freq)
     if int(line_split[1]) < last_freq:
         freq_rank += 1
         last_freq = int(line_split[1])
@@ -15,7 +15,9 @@ for line in file_lines:
     else:
         #this shouldnt happen
         print("sort your frequency list before passing it through")
+    print(line_split[0] + ":" + str(freq_rank))
 
+print("Finished processing\nSaving file")
 
 with open("term_meta_bank_1.json", "w", encoding = "UTF-8") as results_file:
     results_file.write("[")
@@ -25,3 +27,5 @@ with open("term_meta_bank_1.json", "w", encoding = "UTF-8") as results_file:
         if i < list_length - 1:
             results_file.write(",")
     results_file.write("]")
+
+print("Done")
