234bf999eae1002e642cd4b8c035ea420c7e0dc8
kuuuube <hexagonisalie@gmail.com>
2024-06-13 20:31:55 -0400

    Remove unused width aware cookie

--- scripts_and_programs/wikipedia_full_width_always/wikipedia_full_width_always.js
+++ scripts_and_programs/wikipedia_full_width_always/wikipedia_full_width_always.js
@@ -38,7 +38,6 @@ function disableSidebar() {
 
 function setCookie() {
     document.cookie = "enwikimwclientpreferences=vector-feature-appearance-pinned-clientpref-0%2Cvector-feature-limited-width-clientpref-0"
-    document.cookie = "enwikilimited-width-aware=1";
 }
 
 (function() {
