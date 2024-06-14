a4a3ff9b9a097e80eb0c03b70a17498ad6e75233
kuuuube <hexagonisalie@gmail.com>
2024-06-14 17:47:32 -0400

    Add compositionend listener to monkeytype jp fixer

--- scripts_and_programs/monkeytype_jp_fixer/monkeytype_jp_fixer.js
+++ scripts_and_programs/monkeytype_jp_fixer/monkeytype_jp_fixer.js
@@ -1,17 +1,21 @@
 // ==UserScript==
 // @name         Monkeytype JP Fixer
 // @namespace    https://github.com/Kuuuube/Misc_Scripts/tree/main/scripts_and_programs/monkeytype_jp_fixer
-// @version      0.1
+// @version      0.2
 // @description  Automatically inserts a space when after enter is pressed to make typing Japanese less awkward.
 // @author       Kuuube
 // @match        *://monkeytype.com/
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=monkeytype.com
-// @grant        none
 // ==/UserScript==
 
 (function() {
     window.addEventListener("keydown", function(event) {
-        if (event.code === "Enter") {
+        if (event.code === "Enter" && document.getElementById("wordsInput").value.slice(-1) != " ") {
+            document.getElementById("wordsInput").value += " ";
+        }
+    });
+    window.addEventListener("compositionend", function(event) {
+        if (document.getElementById("wordsInput").value.slice(-1) != " ") {
             document.getElementById("wordsInput").value += " ";
         }
     });
