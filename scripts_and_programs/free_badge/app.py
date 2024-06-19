dfd1a98865d17eb32d9a87266c9d002da45a3c33
kuuuube <hexagonisalie@gmail.com>
2024-06-19 14:34:24 -0400

    Add command info logging

--- scripts_and_programs/free_badge/app.py
+++ scripts_and_programs/free_badge/app.py
@@ -18,16 +18,17 @@ async def free_badge(interaction: nextcord.Interaction):
     user_id = str(interaction.user.id)
     user_name = str(interaction.user.name) + "#" + str(interaction.user.discriminator)
     command_name = str(interaction.application_command.name)
-    await interaction.send(fetch_command_info(), ephemeral=True)
-    log_command_usage(user_id, user_name, command_name)
+    command_info = fetch_command_info()
+    await interaction.send(command_info, ephemeral=True)
+    log_command_usage(user_id, user_name, command_name, command_info)
 
 def fetch_command_info():
     command_lines = list(map(str.strip, open("command_output.txt", "r", encoding="UTF-8").readlines()))
     return random.choice(command_lines).replace("\\n", "\n")
 
-def log_command_usage(user_id, user_name, command_name):
+def log_command_usage(user_id, user_name, command_name, command_info):
     date_time_string = str(datetime.datetime.now())
-    info_string = date_time_string + " Command \"" + command_name + "\" used by " + user_id + " " + user_name
+    info_string = date_time_string + " Command \"" + command_name + "\" used by " + user_id + " " + user_name + " Command output: " + command_info
     print(info_string)
     log_file = open("log_file.txt", "a")
     log_file.write(info_string + "\n")
