244cb1161089fefb9b7bf690f9e95d977b0264c3
kuuuube <hexagonisalie@gmail.com>
2024-07-06 22:49:54 -0400

    Optimize python numgen

--- scripts_and_programs/number_generator/python/numgen.py
+++ scripts_and_programs/number_generator/python/numgen.py
@@ -1,16 +1,12 @@
 start_number = input("Start Number: ")
-end_number = input("End Number: ")
+end_number = int(input("End Number: "))
 
 number_prefix = input("Number Prefix: ")
 number_suffix = input("Number Suffix: ")
 
-current_number = start_number
-numberstring = ""
-
-while int(current_number) <= int(end_number):
-    numberstring += (str(number_prefix) + str(current_number) + str(number_suffix))
-    numberstring += ("\n")
-    current_number = int(current_number) + 1
+current_number = int(start_number)
 
 with open ("output.txt", "w") as numfile:
-    numfile.write(numberstring)
+    while current_number <= end_number:
+        numfile.write(number_prefix + str(current_number) + number_suffix + "\n")
+        current_number += 1
