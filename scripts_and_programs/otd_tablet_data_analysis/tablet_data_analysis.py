84896e9b27f066a7db23d323be5842427853736e
kuuuube <hexagonisalie@gmail.com>
2025-10-10 22:05:40 -0400

    Add position state on pressure on/off

--- scripts_and_programs/otd_tablet_data_analysis/tablet_data_analysis.py
+++ scripts_and_programs/otd_tablet_data_analysis/tablet_data_analysis.py
@@ -3,10 +3,12 @@ import sys
 
 args = sys.argv[1:]
 
-pressure_hold_times_off = []
 pressure_hold_times_on = []
-pressure_time_deltas_off = []
 pressure_time_deltas_on = []
+position_state_on_pressure_on = {"match": 0, "change": 0}
+pressure_hold_times_off = []
+pressure_time_deltas_off = []
+position_state_on_pressure_off = {"match": 0, "change": 0}
 skipped_lines = 0
 filtered_lines = 0
 
@@ -15,10 +17,12 @@ line_filter = args[1] if len(args) > 1 else ""
 
 hold_timer_ms = 0
 last_pressure = 0
+last_position = [0, 0]
 with open(data_capture_filename) as tablet_data:
     for line in tablet_data.readlines():
         if line_filter in line:
             pressure_search = re.search(r"(?<=Pressure:)\d+", line)
+            position_search = re.search(r"(?<=Position:\[).*?(?=\])", line)
             timedelta_search = re.search(r"(?<=Delta:)\d+\.?\d*", line)
 
             if not pressure_search or not timedelta_search:
@@ -27,6 +31,7 @@ with open(data_capture_filename) as tablet_data:
 
             pressure = int(pressure_search[0])
             timedelta = float(timedelta_search[0])
+            position = list(map(int, position_search[0].split(",")))
 
             if last_pressure > 0 and pressure == 0:
                 pressure_time_deltas_off.append(timedelta)
@@ -34,15 +39,26 @@ with open(data_capture_filename) as tablet_data:
                 pressure_hold_times_on.append(hold_timer_ms)
                 hold_timer_ms = 0
 
+                if last_position == position:
+                    position_state_on_pressure_off["match"] += 1
+                else:
+                    position_state_on_pressure_off["change"] += 1
+
             if last_pressure == 0 and pressure > 0:
                 pressure_time_deltas_on.append(timedelta)
 
                 pressure_hold_times_off.append(hold_timer_ms)
                 hold_timer_ms = 0
 
+                if last_position == position:
+                    position_state_on_pressure_on["match"] += 1
+                else:
+                    position_state_on_pressure_on["change"] += 1
+
             hold_timer_ms += timedelta
 
             last_pressure = pressure
+            last_position = position
         else:
             filtered_lines += 1
 
@@ -65,6 +81,9 @@ print("Pressure Off Delta: (" + "Min: " + str(min(pressure_time_deltas_off)) + "
 
 print("Min hold time on: " + str(min(pressure_hold_times_on)) + "ms")
 print("Min hold time off: " + str(min(pressure_hold_times_off)) + "ms")
+
+print("Position state on pressure on: (Match count: " + str(position_state_on_pressure_off["match"]) + ", Change count: " + str(position_state_on_pressure_off["change"]))
+print("Position state on pressure off: (Match count: " + str(position_state_on_pressure_on["match"]) + ", Change count: " + str(position_state_on_pressure_on["change"]))
 
 print(splitter_line)
 
